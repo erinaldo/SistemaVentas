9c9ae8999049a7ac0d20b191367ae5ebec89c79e
Creacion del proyecto <lacomputer07@gmail.com>
2020-06-01 12:51:58 -0400

    Listar registros en formulario categoria

--- Sistema/Sistema.Presentacion/FrmCategoria.Designer.cs
+++ Sistema/Sistema.Presentacion/FrmCategoria.Designer.cs
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TabGeneral = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DgvListado = new System.Windows.Forms.DataGridView();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.Seleccionar = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.tabControl1.SuspendLayout();
+            this.TabGeneral.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListado)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.TabGeneral);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(932, 561);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // TabGeneral
+            // 
+            this.TabGeneral.Controls.Add(this.lblTotal);
+            this.TabGeneral.Controls.Add(this.DgvListado);
+            this.TabGeneral.Location = new System.Drawing.Point(4, 22);
+            this.TabGeneral.Name = "TabGeneral";
+            this.TabGeneral.Padding = new System.Windows.Forms.Padding(3);
+            this.TabGeneral.Size = new System.Drawing.Size(924, 535);
+            this.TabGeneral.TabIndex = 0;
+            this.TabGeneral.Text = "Listado";
+            this.TabGeneral.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(924, 535);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mantenimiento";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DgvListado
+            // 
+            this.DgvListado.AllowUserToAddRows = false;
+            this.DgvListado.AllowUserToDeleteRows = false;
+            this.DgvListado.AllowUserToOrderColumns = true;
+            this.DgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvListado.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Seleccionar});
+            this.DgvListado.Location = new System.Drawing.Point(3, 78);
+            this.DgvListado.Name = "DgvListado";
+            this.DgvListado.ReadOnly = true;
+            this.DgvListado.Size = new System.Drawing.Size(906, 423);
+            this.DgvListado.TabIndex = 0;
+            this.DgvListado.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvListado_CellContentClick);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(650, 508);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.TabIndex = 1;
+            this.lblTotal.Text = "Total:";
+            // 
+            // Seleccionar
+            // 
+            this.Seleccionar.HeaderText = "Seleccionar";
+            this.Seleccionar.Name = "Seleccionar";
+            this.Seleccionar.ReadOnly = true;
             // 
             // FrmCategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1086, 623);
+            this.ClientSize = new System.Drawing.Size(953, 580);
+            this.Controls.Add(this.tabControl1);
             this.Name = "FrmCategoria";
             this.Text = "FrmCategoria";
+            this.Load += new System.EventHandler(this.FrmCategoria_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.TabGeneral.ResumeLayout(false);
+            this.TabGeneral.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvListado)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage TabGeneral;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.DataGridView DgvListado;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Seleccionar;
     }
 }
--- Sistema/Sistema.Presentacion/FrmCategoria.cs
+++ Sistema/Sistema.Presentacion/FrmCategoria.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sistema.Negocio;
+
+
 
 namespace Sistema.Presentacion
 {
@@ -15,6 +18,42 @@ namespace Sistema.Presentacion
         public FrmCategoria()
         {
             InitializeComponent();
+        }
+
+        private void Listar()
+        {
+            try
+            {
+                DgvListado.DataSource = NCategoria.Listar();
+                this.Formato();
+                lblTotal.Text = "Total registros: " + Convert.ToString(DgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Formato()
+        {
+            DgvListado.Columns[0].Visible = false;
+            DgvListado.Columns[1].Visible = false;
+            DgvListado.Columns[2].Width =150;
+            DgvListado.Columns[3].Width = 400;
+            DgvListado.Columns[3].HeaderText = "Descripcion";
+            DgvListado.Columns[4].Width = 100;
+
+        }
+
+        private void DgvListado_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FrmCategoria_Load(object sender, EventArgs e)
+        {
+            this.Listar();
         }
     }
 }
