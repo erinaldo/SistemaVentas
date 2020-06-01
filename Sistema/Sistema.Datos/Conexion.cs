15a8cfee5eb96b498036b6ed07c1b8beee504586
Creacion del proyecto <lacomputer07@gmail.com>
2020-06-01 10:12:46 -0400

    Cadena de Conexion con la bese de datos

--- Sistema/Sistema.Datos/Conexion.cs
+++ Sistema/Sistema.Datos/Conexion.cs
@@ -10,28 +10,28 @@ namespace Sistema.Datos
     public class Conexion
     {
 
-        private string Base;
-        private string Servidor;
-        private string Usuario;
-        private string Clave;
-        private bool Seguridad;
-        private static Conexion Con = null;
+        private string Base;// Para almacenar el nombre de la base de datos 
+        private string Servidor;//para indicar donde esta alojada la base de datos 
+        private string Usuario;//usuario para acceder a la BD
+        private string Clave;//clave del usuario  para acceder a la BD
+        private bool Seguridad;//
+        private static Conexion Con = null;// objeto para instanciar la clase conexion 
 
 
-        private Conexion()
+        private Conexion()// constructor de la clase  para enviar valores a las propiedades de la clase 
         {
             this.Base = "dbsistema";
             this.Servidor = "LENOVO-PC";
             this.Usuario = "sa";
             this.Clave = "adrianespinal8297204399";
-            this.Seguridad = true;
+            this.Seguridad = true;// Para utilizar la seguridad de windows 
         }
-        public SqlConnection CrearConexion()
+        public SqlConnection CrearConexion()//
         {
             SqlConnection Cadena = new SqlConnection();
             try
             {
-                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";    
                 if (this.Seguridad)
                 {
                     Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
@@ -49,7 +49,7 @@ namespace Sistema.Datos
             return Cadena;
         }
 
-        public static Conexion getInstancia()
+        public static Conexion getInstancia()// Para Verificar si se tiene una instancia de esta clase 
         {
             if (Con == null)
             {
